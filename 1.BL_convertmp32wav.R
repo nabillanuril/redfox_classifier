abd3ccafb3863ea2f32928c02ea9a7b3ce3eca00
nabillanuril <100427459+nabillanuril@users.noreply.github.com>
2025-08-22 09:30:07 +0100

    Update 1.BL_convertmp32wav.R

--- 1.BL_convertmp32wav.R
+++ 1.BL_convertmp32wav.R
@@ -3,10 +3,11 @@ library(tidyverse)
 
 setwd("F:/MSc Ecology & Data Science Research/1. Britishlib_red fox")
 
-# Convert each file and place it into the new folder
-# list files with ".mp3" file extension with the path directory
+# List files with ".mp3" file extension
 mp3_files <- list.files(pattern = "\\.mp3$", full.names = TRUE)
+# Build output .wav paths
 wav_files <- file.path(sub("\\.mp3$", ".wav", basename(mp3_files)))
+# Convert each MP3 to WAV
 for (i in seq_along(mp3_files)) {
   try(av_audio_convert(mp3_files[i], output = wav_files[i]))}
-file.remove(mp3_files)
+
